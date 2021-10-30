ebd32f6b8cf8aa27a64acadd4839d9112e122bba
Stackbit Code Editor <projects@stackbit.com>
2021-10-30 22:39:08 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -6,7 +6,7 @@ import TestimonialsSection from './testimonials'
 registerComponents({
     // Register all Stackbit components
     ...componentsMap
-
+    TestimonialsSection
     // Override any static or dynamic component,
     // or register your own dynamic component.
 });
