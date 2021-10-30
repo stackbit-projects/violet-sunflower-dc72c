c25b5849a212df7c2280e5b7e829c92008769a4f
Stackbit Code Editor <projects@stackbit.com>
2021-10-30 22:36:20 +0000

    testimonials.js: changed

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,5 +1,5 @@
 const TestimonialsSection = (props) => {
-  const { annotationPrefix, logos, title } = props;
+  const { annotationPrefix, tesimonials, title } = props;
   return (
     <div
       className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
