4a301749fde5724bcb522f656327a66b5389998c
Stackbit Code Editor <projects@stackbit.com>
2021-10-30 22:37:41 +0000

    testimonials.js: changed

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,34 +1,34 @@
 const TestimonialsSection = (props) => {
-  const { annotationPrefix, testimonial, title } = props;
-  return (
-    <div
-      className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
-      data-sb-field-path={annotationPrefix}>
-      <h1
-        className="text-3xl tracking-tight sm:text-4xl mb-2"
-        data-sb-field-path=".title">
-        {title}
-      </h1>
-      <div
-        className="flex justify-center items-center"
-        data-sb-field-path=".testimonials">
-        {testimonials.map((testimonial, index) => (
-          <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
-            <img
-              className="mb-2"
-              height="60px"
-              width="60px"
-              src={testimonial.image}
-              data-sb-field-path=".image#@src"
-            />
-            <h2 className="text-sm text-gray-400" data-sb-field-path=".name">
-              {testimonial.name}
-            </h2>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    const { annotationPrefix, testimonial, title } = props;
+    return (
+        <div
+            className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
+            data-sb-field-path={annotationPrefix}>
+            <h1
+                className="text-3xl tracking-tight sm:text-4xl mb-2"
+                data-sb-field-path=".title">
+                {title}
+            </h1>
+            <div
+                className="flex justify-center items-center"
+                data-sb-field-path=".testimonials">
+                {testimonials.map((testimonial, index) => (
+                    <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
+                        <img
+                            className="mb-2"
+                            height="60px"
+                            width="60px"
+                            src={testimonial.image}
+                            data-sb-field-path=".image#@src"
+                        />
+                        <h2 className="text-sm text-gray-400" data-sb-field-path=".name">
+                            {testimonial.name}
+                        </h2>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default TestimonialsSection;
